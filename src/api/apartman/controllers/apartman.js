c98c6bacff50f2f6f70eeb0d90118785fb8dc31b
prismaplatform <contact@theprismaplatform.com>
2024-04-05 14:28:41 +0300

    changeing apartmans and blog-posts seraching index from id, to slug

--- src/api/apartman/controllers/apartman.js
+++ src/api/apartman/controllers/apartman.js
@@ -6,4 +6,16 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::apartman.apartman');
+module.exports = createCoreController("api::apartman.apartman", ({ strapi }) => ({
+    async findOne(ctx) {
+        const { id } = ctx.params;
+
+        const entity = await strapi.db.query("api::apartman.apartman").findOne({
+            where: { slug: id },
+        });
+        const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+
+        return this.transformResponse(sanitizedEntity);
+        },
+    })
+);
--- src/api/blog-post/controllers/blog-post.js
+++ src/api/blog-post/controllers/blog-post.js
@@ -6,4 +6,16 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::blog-post.blog-post');
+module.exports = createCoreController("api::blog-post.blog-post", ({ strapi }) => ({
+    async findOne(ctx) {
+        const { id } = ctx.params;
+
+        const entity = await strapi.db.query("api::blog-post.blog-post").findOne({
+            where: { slug: id },
+        });
+        const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+
+        return this.transformResponse(sanitizedEntity);
+        },
+    })
+);
